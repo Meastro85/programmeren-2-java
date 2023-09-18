88fe73746e86d85291de12661d91304573801bcf
Meastro85 <verboven85@gmail.com>
2023-09-18 08:56:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Groeiproject Programmeren 2 - Java
+
+* Naam: Vincent Verboven
+* Klasgroep: INF202A
+* Studentennummer:
+* Academiejaar: 2023-2024
---
+++ m1_herhaling/src/main/java/Demo1.java
@@ -0,0 +1,9 @@
+/**
+ * Vincent Verboven
+ * 18/09/2023
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("demo");
+    }
+}
